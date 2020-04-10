db7759f3c8c53d96ddf7cab7db03c9b80f375188
Riadulislam909 <riadulislam814@gmail.com>
2020-04-10 19:17:24 +0600

    Admin can edit book

--- app/Http/Controllers/AdminPagesController.php
+++ app/Http/Controllers/AdminPagesController.php
@@ -23,7 +23,7 @@ class AdminPagesController extends Controller
 
 
         $request->validate([
-            'title' => 'required|unique|max:150',
+            'title' => 'required|max:150',
             'description' => 'required',
             'price' => 'required|numeric',
             'quantity' => 'required|numeric',
@@ -61,4 +61,57 @@ class AdminPagesController extends Controller
       return redirect()->route('admin.product.create');
         
     }
+
+    public function product_update(Request $request, $id){
+
+
+        $request->validate([
+            'title' => 'required|max:150',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'quantity' => 'required|numeric',
+            
+        ]);
+
+      $product = Product::find($id);
+
+      $product->title = $request->title;
+      $product->description = $request->description;
+      $product->price = $request->price;
+      $product->quantity = $request->quantity;
+
+      
+      $product->save();
+
+      if($request->hasFile('product_image')){
+          $image = $request->file('product_image');
+          $img = time(). '.'. $image->getClientOriginalExtension();
+          $location = public_path('images/products/' .$img);
+          Image::make($image)->save($location);
+
+
+          //$product_image = new ProductImage;
+          //$product_image->product_id = $product->id;
+          //$product_image->image = $img;
+          //$product_image->save();
+
+      }
+      
+
+      return redirect()->route('admin.products');
+        
+    }
+     
+    
+    public function manage_products(){
+
+        $products = Product::orderBy('id','desc')->get();
+        return view('admin.pages.product.index')->with('products', $products);
+    }
+    public function product_edit($id){
+
+        $product = Product::find($id);
+        return view('admin.pages.product.edit')->with('product', $product);
+    }
+
 }
