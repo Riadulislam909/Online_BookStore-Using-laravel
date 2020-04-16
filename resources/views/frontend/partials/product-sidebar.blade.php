46a1283486e0c8af415eec28701de186fd4ba701
Riadulislam909 <riadulislam814@gmail.com>
2020-04-17 01:28:01 +0600

    Categories side bar

--- resources/views/frontend/partials/product-sidebar.blade.php
+++ resources/views/frontend/partials/product-sidebar.blade.php
@@ -1,5 +1,18 @@
 <div class="list-group">
-          <a href="#" class="list-group-item list-group-item-action">First item</a>
-          <a href="#" class="list-group-item list-group-item-action">Second item</a>
-          <a href="#" class="list-group-item list-group-item-action">Third item</a>
+    @foreach(App\Models\Category::orderBy('name','asc')->where('parent_id',NULL)->get() as $parent)
+          <a href="#main-{{ $parent->id }}" class="list-group-item list-group-item-action" data-toggle="collapse">
+          <img src="{!! asset('images/categories/'.$parent->image) !!}" width="50">
+          {{ $parent->name}}
+          </a>
+          <div class="child-rows" id="main-{{ $parent->id }}">
+          @foreach(App\Models\Category::orderBy('name','asc')->where('parent_id', $parent->id)->get() as $child)
+          <a href="#main-{{ $parent->id }}" class="list-group-item list-group-item-action" data-toggle="collapse">
+          <img src="{!! asset('images/categories/'.$child->image) !!}" width="25">
+          {{ $child->name}}
+          </a>
+          
+          @endforeach
+        
+          </div>
+          @endforeach
         </div>
